4d4c9e2187c39b8d08d86f6812304f9f6f97ef39
Urja Rannikko <urjaman@gmail.com>
2016-07-19 00:11:27 +0300

    Fixup iso-as-usb booting

--- iso.sh
+++ iso.sh
@@ -10,3 +10,5 @@ mkdir -p iso/boot/grub
 cp -aL boot/* iso/boot
 cp grub.cfg iso/boot/grub/grub.cfg
 grub2-mkrescue -o $GENNAME.iso iso
+# Genkernel scripts will not boot off an usb-stick-as-iso if it has any partitions, so the helpful 0xcd partition by grub sadly has to go :(
+printf "d\nw\ny\n" | fdisk $GENNAME.iso
